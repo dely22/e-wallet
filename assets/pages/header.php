9482961e88c7e342cf0c89c2d7bcb12ff0e9f833
dely22 <almktaryh@gmail.com>
2022-03-13 03:40:46 -0800

    full login auth / issues in some op -_-

---
+++ assets/pages/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" >
+    <!-- <?=site_url('assets/bootstrap/css/bootstrap.min.css')?>" -->
+    
+    <title><?=$page_title?></title>
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-primary shadow-sm">
+  <div class="container">
+      
+    <a class="navbar-brand" href="#">
+      <img src="assets/images/wallet.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      dely-Wallet
+    </a>
+    <?php
+if(isset($_SESSION['Auth'])){ ?>
+  <div class="">
+  <a class="btn btn-sm btn-dark" href="assets/php/process.php?logout">Logout</a>
+  
+</div> 
+<?php
+}else{
+  ?>
+ <div class="">
+        <a class="btn btn-sm btn-dark" href="?login">Login</a>
+        &nbsp;&nbsp;
+        <a class="btn btn-sm btn-dark" href="?signup">Signup</a>
+      </div>
+  <?php
+}
+    ?>
+   
+  </div>
+</nav>
